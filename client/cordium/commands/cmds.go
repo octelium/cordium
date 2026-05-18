6e8da593750f341cd9d20838b8ec321769f4a4a8
George B. <george@octelium.com>
2026-05-18 12:40:41 +0000

    client/cordium - remove code cmd for now

--- client/cordium/commands/cmds.go
+++ client/cordium/commands/cmds.go
@@ -18,7 +18,6 @@ package commands
 
 import (
 	"github.com/octelium/cordium/client/cordium/commands/build"
-	"github.com/octelium/cordium/client/cordium/commands/code"
 	"github.com/octelium/cordium/client/cordium/commands/cp"
 	"github.com/octelium/cordium/client/cordium/commands/create"
 	"github.com/octelium/cordium/client/cordium/commands/delete"
@@ -65,7 +64,7 @@ func InitCmds() {
 
 	Cmd.AddCommand(ssh.Cmd)
 	Cmd.AddCommand(cp.Cmd)
-	Cmd.AddCommand(code.Cmd)
+	// Cmd.AddCommand(code.Cmd)
 	Cmd.AddCommand(auth.Cmd)
 	Cmd.AddCommand(terminal.Cmd)
 	Cmd.AddCommand(exec.Cmd)
